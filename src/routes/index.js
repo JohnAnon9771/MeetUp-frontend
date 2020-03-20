8647d2e4d7440541dc7c03da48e0f2f77529b23e
JohnAnon9771 <njoao97710@gmail.com>
2020-03-20 19:33:30 -0300

    Added new pages

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,15 +2,16 @@ import React from 'react';
 import { Switch } from 'react-router-dom';
 import Route from './Route';
 
-import SignIn from '../pages/signIn';
-import SignUp from '../pages/signUp';
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
+import Dashboard from '../pages/Dashboard';
 
 export default function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={SignIn} />
       <Route path="/register" component={SignUp} />
-      {/* <Route path="" component={} /> */}
+      <Route path="/dashboard" component={Dashboard} />
     </Switch>
   );
 }
