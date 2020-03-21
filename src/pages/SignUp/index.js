a06cbdc4783dff79b278d0732d7200366d2e04b3
JohnAnon9771 <njoao97710@gmail.com>
2020-03-20 22:19:52 -0300

    Added structure for redux and redux saga

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,5 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { Form } from '@unform/web';
+import Input from '../../components/Input';
+
+import logo from '../../assets/logo.svg';
 
 export default function SignUp() {
-  return <div />;
+  function handleSubmit(data) {}
+  return (
+    <>
+      <img src={logo} alt="" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="name" type="text" placeholder="Digite seu nome completo" />
+        <Input name="email" type="email" placeholder="Digite seu e-mail" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Sua senha secreta"
+        />
+        <button type="submit">Cadastrar</button>
+      </Form>
+      <Link to="/">Já tenho login</Link>
+    </>
+  );
 }
---
+++ src/store/modules/rootReducer.js
@@ -0,0 +1,5 @@
+import { combineReducers } from 'redux';
+
+import meetup from './meetups/reducer';
+
+export default combineReducers({ meetup });
